320e206e292e62cf0ab34380f01f0fa0303617b7
hzwangjl <wjl_hdu@163.com>
2019-07-30 16:46:42 +0800

    add information entropy

--- predict.py
+++ predict.py
@@ -20,9 +20,19 @@ def data_process(x):
     std = torch.tensor(std, dtype=torch.float32)
     return x.sub_(mean[:, None, None]).div_(std[:, None, None])
 
+def info_entropy(res, h, w):
+    print(res.shape)
+    tensor = torch.from_numpy(res).to("cuda")
+    entropy = tensor[0,:,:]*torch.log(tensor[0,:,:]) + \
+        tensor[1,:,:]*torch.log(tensor[1,:,:]) + \
+        tensor[2,:,:]*torch.log(tensor[2,:,:])
+    entropy = torch.sum(entropy)
+    entropy = (-entropy / (w * h)).to("cpu")
+
+    return entropy
 
 if __name__ == '__main__':
-    Vis = False
+    Vis = True
     SaveTxt = False
     nb_classes = 3
 
@@ -35,7 +45,6 @@ if __name__ == '__main__':
 
     model.eval()
 
-    #img_path = 'training_data/val/imgs/'
     img_path = '/home/shining3d/Datasets/teethdataset/new_test_data_20190725_180/images/'
     mask_path = '/home/shining3d/Datasets/teethdataset/new_test_data_20190725_180/masks/'
 
@@ -44,7 +53,7 @@ if __name__ == '__main__':
     Ltime = []
     tTime_start = time.time()
 
-    mode = "val"
+    mode = "test"
     if mode == "test":
         for iter, img_file in enumerate(img_folder):
             if iter % 500 == 0:
@@ -64,6 +73,9 @@ if __name__ == '__main__':
                 os.makedirs("save_txt", exist_ok=True)
                 np.savetxt((os.path.join("save_txt", img_file.split('.')[0] + '.txt')), y1, fmt='%d')
             if Vis:
+                entropy = info_entropy(numpy_y, image.size[0], image.size[1])
+                plt.suptitle("Information Entroy: %.02f"%(entropy))
+
                 plt.subplot(1, 2, 1)
                 plt.imshow(image)
                 plt.subplot(1, 2, 2)
